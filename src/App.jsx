dda3de181d01bf9131855205c863b808fa5df3a0
Abin Babu <abinbabuonline@gmail.com>
2024-09-17 12:51:11 +0530

    routing completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { Suspense } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from "./pages/Layout";
+import AddProduct from "./pages/AddProduct.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
 
-  return (
+const ProductDetails = lazy(() => import("./pages/ProductDetails.jsx"));
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Route path="/" element={<Layout />}>
+        <Route path="" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+        <Route
+          path="product-details/:id"
+          element={
+            <Suspense
+              fallback={<h1 className="text-center font-bold">Loading</h1>}
+            >
+              <ProductDetails />
+            </Suspense>
+          }
+        />
+      </Route>
+      <Route path="/sell" element={<Sell/>}/>
+      <Route path="/add-product" element={<AddProduct/>}/>
     </>
   )
-}
+);
 
-export default App
+const App = () => {
+  return (
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  );
+};
+
+export default App;
